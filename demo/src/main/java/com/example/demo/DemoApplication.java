102c4698fe158239ae90c4269b9f6ab71dbfd043
86173 <86173@wzh>
2019-07-22 22:46:05 +0800

    DD

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("112666221");
+		System.out.println("44");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
