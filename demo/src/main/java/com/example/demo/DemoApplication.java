aaef2a838ff407fe051beb716aaca1f9bafa62e5
86173 <86173@wzh>
2019-07-22 22:37:26 +0800

    ee

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("111");
+		System.out.println("112221");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
